9e2d5cf1772d01dc105544ead57cc2e838621465
comnows <watthanandh.l@mail.kmutt.ac.th>
2021-07-13 19:47:26 +0700

    Add stage tile & change some code

--- First2DGameProject_Practice/Assets/Scripts/Movement.cs
+++ First2DGameProject_Practice/Assets/Scripts/Movement.cs
@@ -4,25 +4,52 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField]
-    Animator playerAnim;
+    [SerializeField] Animator playerAnim;
+    [SerializeField] Rigidbody2D playerRB;
+    [SerializeField] float moveSpeed = 5;
+    //[Range(0,.3f)][SerializeField] float movementSmooth = 0.05f;
 
-    [SerializeField]
-    Rigidbody2D playerRB;
+    //float horizontalMove = 0f;
+    //bool isJump = false;
 
-    [SerializeField]
-    float moveSpeed = 5;
+    //private bool facingRight = false;
+    //private Vector3 velocity = Vector3.zero;
 
     private void Update() 
     {
         if(Input.GetKey(KeyCode.A))
         {
-            playerRB.velocity = new Vector2(-moveSpeed, playerRB.velocity.y);
-        }
-
-        if(Input.GetKey(KeyCode.D))
-        {
             playerRB.velocity = new Vector2(moveSpeed, playerRB.velocity.y);
         }
     }
+
+    //advance see in Brackeys Channel
+    /*private void FixedUpdate() 
+    {
+        playerRB.velocity = new Vector2(horizontalMove * 10f, playerRB.velocity.y);
+    }*/
+
+    /*void Move(float move)
+    {
+        Vector3 targetVelocity = new Vector2(move * 10f, playerRB.velocity.y);
+        playerRB.velocity = Vector3.SmoothDamp(playerRB.velocity, targetVelocity, ref velocity, movementSmooth);
+
+        if(move > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if(move < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }*/
 }
